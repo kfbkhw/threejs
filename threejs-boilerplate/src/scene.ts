6fd7e541c41a4a24ed54fc4d8d1f870f04aa0a01
Hailey Kim <kfbkhw@gmail.com>
2024-03-16 20:57:20 +0900

    create threejs boilerplate

---
+++ threejs-boilerplate/src/scene.ts
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+
+export default function scene(node: HTMLDivElement) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    node.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x378ce7 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.set(0, 0, 5);
+    camera.lookAt(cube.position);
+
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+}
