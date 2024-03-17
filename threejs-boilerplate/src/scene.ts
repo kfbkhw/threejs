f017309b190050437982d5b97e464380ad12c478
Hailey Kim <kfbkhw@gmail.com>
2024-03-17 20:42:37 +0900

    update boilerplate

--- threejs-boilerplate/src/scene.ts
+++ threejs-boilerplate/src/scene.ts
@@ -1,6 +1,10 @@
 import * as THREE from 'three';
 
 export default function scene(node: HTMLDivElement) {
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    node.appendChild(renderer.domElement);
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
         75,
@@ -8,10 +12,6 @@ export default function scene(node: HTMLDivElement) {
         0.1,
         1000
     );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    node.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x378ce7 });
@@ -21,9 +21,25 @@ export default function scene(node: HTMLDivElement) {
     camera.position.set(0, 0, 5);
     camera.lookAt(cube.position);
 
+    const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 5);
+    directionalLight.position.set(-5, 8, 5);
+    scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+    scene.add(ambientLight);
+
     function animate() {
         requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         renderer.render(scene, camera);
     }
     animate();
+
+    function resize() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+    window.addEventListener('resize', resize);
 }
