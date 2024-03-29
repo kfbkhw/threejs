5818d8bc496ef4d8a01efaed0d6abe5a0cefc2d5
Hailey Kim <kfbkhw@gmail.com>
2024-03-29 17:41:44 +0900

    update: add rings and pole

--- starlight-earth/src/scene.ts
+++ starlight-earth/src/scene.ts
@@ -48,6 +48,7 @@ export default function scene(node: HTMLDivElement) {
             side: THREE.BackSide,
         });
         const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateZ(Math.PI * -0.1);
         scene.add(mesh);
         return mesh;
     };
@@ -61,6 +62,45 @@ export default function scene(node: HTMLDivElement) {
             side: THREE.FrontSide,
         });
         const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateZ(Math.PI * -0.1);
+        scene.add(mesh);
+        return mesh;
+    };
+
+    const createPole = () => {
+        const geometry = new THREE.CylinderGeometry(0.005, 0.005, 4.8);
+        const material = new THREE.MeshStandardMaterial({
+            transparent: true,
+            opacity: 0.5,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateZ(Math.PI * -0.1);
+        scene.add(mesh);
+        return mesh;
+    };
+
+    const createPoleRing = () => {
+        const geometry = new THREE.TorusGeometry(2.4, 0.005, 120, 480, Math.PI);
+        const material = new THREE.MeshStandardMaterial({
+            transparent: true,
+            opacity: 0.5,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateZ(Math.PI * -0.6);
+        scene.add(mesh);
+        return mesh;
+    };
+
+    const createRing = () => {
+        const geometry = new THREE.TorusGeometry(2.6, 0.005, 120, 480);
+        const material = new THREE.MeshStandardMaterial({
+            transparent: true,
+            opacity: 0.5,
+            color: 0xf6d776,
+        });
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotateX(Math.PI / 2);
+        mesh.rotateZ(Math.PI / 2);
         scene.add(mesh);
         return mesh;
     };
@@ -94,6 +134,9 @@ export default function scene(node: HTMLDivElement) {
 
     const earthLg = createEarthLg(earthTexture);
     const earthSm = createEarthSm(earthTexture);
+    createPole();
+    createPoleRing();
+    const ring = createRing();
     const stars = createStars(starTexture, 1000);
 
     // -------------- Lights -------------- //
@@ -105,10 +148,10 @@ export default function scene(node: HTMLDivElement) {
         requestAnimationFrame(render);
         renderer.render(scene, camera);
         control.update();
-        earthLg.rotation.x += 0.0005;
         earthLg.rotation.y += 0.0005;
-        earthSm.rotation.x -= 0.0005;
         earthSm.rotation.y -= 0.0005;
+        ring.rotation.x -= 0.005;
+        ring.rotation.y -= 0.005;
         stars.rotation.x += 0.0001;
         stars.rotation.y += 0.0001;
         stars.rotation.z += 0.0001;
